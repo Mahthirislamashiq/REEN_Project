22b2406b27f23c04fa451b5cbf30fada61a5d4d2
Md Mahthir Islam <63516692+Mahthirislamashiq@users.noreply.github.com>
2020-08-20 12:07:59 +0600

    Responsive Design REEN Project

---
+++ js/coustom.js
@@ -0,0 +1,25 @@
+/********************** 
+################ owl carousel Start ###########
+***********************/ 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+/********************** 
+################ owl carousel End ###########
+***********************/ 
